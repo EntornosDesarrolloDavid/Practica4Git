b129a8c8516eed94bd36ac53a6335f6183a39666
maria <maria@LAPTOP-U0QNUSCL>
2021-05-11 19:40:55 +0200

    Añadir una tercera clase y modificar la segunda

--- prGit/src/prGit/SegundaClase.java
+++ prGit/src/prGit/SegundaClase.java
@@ -3,8 +3,7 @@ package prGit;
 public class SegundaClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("He modificado esta clase");
 	}
 
 }
---
+++ prGit/src/prGit/TerceraClase.java
@@ -0,0 +1,10 @@
+package prGit;
+
+public class TerceraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
